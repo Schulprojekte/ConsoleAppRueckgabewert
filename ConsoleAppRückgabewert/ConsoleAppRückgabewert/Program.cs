53f7b780abc804f2d9b2b21b576c24d7ec7d3c15
David Hülsberg <46696514+david-h-dev@users.noreply.github.com>
2020-08-17 17:01:04 +0200

    Initial commit

---
+++ ConsoleAppRückgabewert/ConsoleAppRückgabewert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRückgabewert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(MethodeRückgabewert(20, 30));
+            //Console.ReadKey();
+
+            Console.WriteLine("Geben Sie eine ganze Zahl ein");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben Sie eine ganze Zahl ein");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Summe: " + MethodeRückgabewert(a, b));
+        }
+
+        private static int x, y, z;
+
+        private static int MethodeRückgabewert(int zahl1, int zahl2) //Eingabe
+        {
+            x = zahl1;
+            y = zahl2;
+            z = x + y;
+            return z;
+        }
+    }
+}
